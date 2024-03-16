4b67ab3d2153d085006bdceccd8c2fe50ac2920d
Bob Hart <77585854+bhart17@users.noreply.github.com>
2024-03-16 15:31:50 +0000

    redirect fix

---
+++ src/routes/api/auth/+server.ts
@@ -0,0 +1,35 @@
+import type { RequestHandler } from "./$types";
+
+import { error, json } from "@sveltejs/kit";
+import type { CookieSerializeOptions } from "cookie";
+
+import { adminAuth } from "$lib/firebase/server";
+
+export const POST: RequestHandler = async ({ request, cookies }) => {
+    const { idToken } = await request.json();
+
+    const expiresIn = 1000 * 60 * 60 * 24 * 7; // 7 days
+
+    const decodedIdToken = await adminAuth.verifyIdToken(idToken);
+
+    if (new Date().getTime() / 1000 - decodedIdToken.auth_time >= 5 * 60)
+        throw error(401, "Recent sign in required");
+
+    const cookie = await adminAuth.createSessionCookie(idToken, { expiresIn });
+
+    const options: CookieSerializeOptions = {
+        maxAge: expiresIn,
+        httpOnly: true,
+        secure: true,
+        path: '/',
+    };
+    
+    cookies.set("__session", cookie, options);
+
+    return json({ status: "signedIn" });
+};
+
+export const DELETE: RequestHandler = async ({ cookies }) => {
+    cookies.delete("__session", { path: '/' });
+    return json({ status: "signedOut" });
+};

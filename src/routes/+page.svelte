d94366975971081099185df572d0b1cdeb675a99
Kayla <kml02@hotmail.co.uk>
2024-03-16 17:39:30 +0000

    commit made

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,9 +3,27 @@
     import { user } from "$lib/firebase/client/auth";
 </script>
 
-<h1 class="text-3xl font-bold underline">Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
-
+<div class="navbar bg-base-100">
+    <div class="flex-1">
+      <a class="btn btn-ghost text-xl">FabricFusion</a>
+    </div>
+    <div class="flex-none">
+      <ul class="menu menu-horizontal px-1">
+        <li><a>Log In</a></li>
+        <li>
+          <details>
+            <summary>
+              Parent
+            </summary>
+            <ul class="p-2 bg-base-100 rounded-t-none">
+              <li><a>Link 1</a></li>
+              <li><a>Link 2</a></li>
+            </ul>
+          </details>
+        </li>
+      </ul>
+    </div>
+  </div>
 
 current user: {$user?.email}
 <button class="btn" on:click={() => {signIn("user@test.com", "password")}}>Login</button>
@@ -14,3 +32,99 @@ current user: {$user?.email}
 <button>
     <a href = "/login">Go to login page</a>
 </button>
+<h1 class="text-3xl font-bold">Currently Watching</h1>
+<div class="flex w-full">
+    <div class="card card-compact w-96 bg-base-100 shadow-xl">
+        <figure><img src="https://www.akeneo.com/wp-content/uploads/2023/09/AdobeStock_589899369-scaled.jpeg" alt="Sustainable clothing logo" /></figure>
+        <div class="card-body">
+          <h2 class="card-title">Project: Insert project title</h2>
+          <p>Project description</p>
+          <div class="card-actions justify-end">
+            <button class="btn btn-primary">View</button>
+          </div>
+        </div>
+      </div>
+    <div class="divider divider-horizontal"></div>
+    <div class="card card-compact w-96 bg-base-100 shadow-xl">
+        <figure><img src="https://www.akeneo.com/wp-content/uploads/2023/09/AdobeStock_589899369-scaled.jpeg" alt="Sustainable clothing logo" /></figure>
+        <div class="card-body">
+          <h2 class="card-title">Project: Insert project title</h2>
+          <p>Project description</p>
+          <div class="card-actions justify-end">
+            <button class="btn btn-primary">View</button>
+          </div>
+        </div>
+      </div>
+      <div class="divider divider-horizontal"></div>
+      <div class="card card-compact w-96 bg-base-100 shadow-xl">
+        <figure><img src="https://www.akeneo.com/wp-content/uploads/2023/09/AdobeStock_589899369-scaled.jpeg" alt="Sustainable clothing logo" /></figure>
+        <div class="card-body">
+          <h2 class="card-title">Project: Insert project title</h2>
+          <p>Project description</p>
+          <div class="card-actions justify-end">
+            <button class="btn btn-primary">View</button>
+          </div>
+        </div>
+      </div>
+    <div class="divider divider-horizontal"></div>
+    <div class="card card-compact w-96 bg-base-100 shadow-xl">
+        <figure><img src="https://www.akeneo.com/wp-content/uploads/2023/09/AdobeStock_589899369-scaled.jpeg" alt="Sustainable clothing logo" /></figure>
+        <div class="card-body">
+          <h2 class="card-title">Project: Insert project title</h2>
+          <p>Project description</p>
+          <div class="card-actions justify-end">
+            <button class="btn btn-primary">View</button>
+          </div>
+        </div>
+      </div>
+  </div>
+
+<button>
+    <a href = "/login">Go to login page</a>
+</button>
+<h1 class="text-3xl font-bold">Currently Watching</h1>
+<div class="flex w-full">
+    <div class="card card-compact w-96 bg-base-100 shadow-xl">
+        <figure><img src="https://www.akeneo.com/wp-content/uploads/2023/09/AdobeStock_589899369-scaled.jpeg" alt="Sustainable clothing logo" /></figure>
+        <div class="card-body">
+          <h2 class="card-title">Project: Insert project title</h2>
+          <p>Project description</p>
+          <div class="card-actions justify-end">
+            <button class="btn btn-primary">View</button>
+          </div>
+        </div>
+      </div>
+    <div class="divider divider-horizontal"></div>
+    <div class="card card-compact w-96 bg-base-100 shadow-xl">
+        <figure><img src="https://www.akeneo.com/wp-content/uploads/2023/09/AdobeStock_589899369-scaled.jpeg" alt="Sustainable clothing logo" /></figure>
+        <div class="card-body">
+          <h2 class="card-title">Project: Insert project title</h2>
+          <p>Project description</p>
+          <div class="card-actions justify-end">
+            <button class="btn btn-primary">View</button>
+          </div>
+        </div>
+      </div>
+      <div class="divider divider-horizontal"></div>
+      <div class="card card-compact w-96 bg-base-100 shadow-xl">
+        <figure><img src="https://www.akeneo.com/wp-content/uploads/2023/09/AdobeStock_589899369-scaled.jpeg" alt="Sustainable clothing logo" /></figure>
+        <div class="card-body">
+          <h2 class="card-title">Project: Insert project title</h2>
+          <p>Project description</p>
+          <div class="card-actions justify-end">
+            <button class="btn btn-primary">View</button>
+          </div>
+        </div>
+      </div>
+    <div class="divider divider-horizontal"></div>
+    <div class="card card-compact w-96 bg-base-100 shadow-xl">
+        <figure><img src="https://www.akeneo.com/wp-content/uploads/2023/09/AdobeStock_589899369-scaled.jpeg" alt="Sustainable clothing logo" /></figure>
+        <div class="card-body">
+          <h2 class="card-title">Project: Insert project title</h2>
+          <p>Project description</p>
+          <div class="card-actions justify-end">
+            <button class="btn btn-primary">View</button>
+          </div>
+        </div>
+      </div>
+  </div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,10 +2,35 @@
 export default {
 	content: ['./src/**/*.{html,js,svelte,ts}'],
 	theme: {
-		extend: {}
+		extend: {},
 	},
 	plugins: [require('daisyui')],
 	daisyui: {
-		logs: false
+		logs: false,
+		themes: [
+			{
+			mytheme: {
+				
+				"primary": "#009eb8",
+          
+				"secondary": "#007b00",
+						 
+				"accent": "#00aaac",
+						 
+				"neutral": "#1e282f",
+						 
+				"base-100": "#043127",
+						 
+				"info": "#00a7c2",
+						 
+				"success": "#00c368",
+						 
+				"warning": "#ff6400",
+						 
+				"error": "#ff97ae",
+
+				},
+			  },
+			],
+		},
 	}
-};

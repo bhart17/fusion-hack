0f4d0866d1874f3d0dbcdc7ea251cb946d802fdd
Bob Hart <77585854+bhart17@users.noreply.github.com>
2024-03-17 05:36:29 +0000

    home performance

--- src/routes/(app)/home/+page.server.ts
+++ src/routes/(app)/home/+page.server.ts
@@ -1,37 +1,40 @@
 import type { PageServerLoad } from './$types';
-import type { ProjectDoc } from '$lib/firebase/types';
 import { firestore } from '$lib/firebase/server/firestore';
+import { error } from '@sveltejs/kit';
 
 export const load = (async ({ locals }) => {
 	const projects: {
 		watching: { title: string; description: string; image: string; id: string }[];
 		hosted: { title: string; description: string; image: string; id: string }[];
 	} = { watching: [], hosted: [] };
+
 	if (!locals.user) return projects;
+
 	const userDoc = (await firestore.users.doc(locals.user.uid).get()).data();
-	// console.log(userDoc)
-	for (const hostedProject of userDoc?.hostprojects ?? []) {
-		console.log();
-		const project = (await firestore.projects.doc(hostedProject).get()).data() as ProjectDoc;
+	if (!userDoc) error(500, 'User not found');
+
+	await Promise.all(userDoc.hostprojects.map(async (hostedProject) => {
+		const project = (await firestore.projects.doc(hostedProject).get()).data();
+		if (!project) return;
 		projects.hosted.push({
 			id: hostedProject,
 			title: project.title,
 			description: project.description,
 			image: project.image
 		});
-	}
-	for (const watchingProject of userDoc?.watching ?? []) {
-		const project = (await firestore.projects.doc(watchingProject).get()).data() as ProjectDoc;
+	}));
+
+	await Promise.all(userDoc.watching.map(async (watchingProject) => {
+		const project = (await firestore.projects.doc(watchingProject).get()).data();
+		if (!project) return;
 		projects.watching.push({
 			id: watchingProject,
 			title: project.title,
 			description: project.description,
 			image: project.image
 		});
-	}
-
-	// console.log(projects);
-	// await firestore.projects.get();
+	}));
+	
 	return projects;
 }) satisfies PageServerLoad;
 

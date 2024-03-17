2c1815dc85dd4bcf905f98df31d5c9c2b52253c2
Bob Hart <77585854+bhart17@users.noreply.github.com>
2024-03-17 07:48:59 +0000

    merge conflict

--- src/routes/(app)/[project=project]/+page.svelte
+++ src/routes/(app)/[project=project]/+page.svelte
@@ -7,9 +7,6 @@
 	import { goto } from '$app/navigation';
 
 	$: watching = $userData?.watching.includes(data.id);
-	
-	let items = data.project?.items;
-
 
 	async function watchProject() {
 		await updateDoc(doc(firestore.users, $user?.uid), { watching: arrayUnion(data.id) });
@@ -20,68 +17,73 @@
 	}
 
 </script>
+
 <svelte:head>
 	<title>Project Details</title>
 	<meta name="description" content="Project Details" />
 </svelte:head>
 
-<div class="flex flex-col items-center gap-4 py-4">
-	<div class="card card-compact w-3/6 bg-base-100 shadow-xl">
+<div class="flex flex-col items-center gap-4 p-4">
+	<div class="card max-w-xl w-full bg-base-100 shadow-xl">
 		<figure>
-			<img src={data.project?.image} alt="User" />
+			{#if data.project?.image === ''}
+						<img
+							src={'https://as2.ftcdn.net/v2/jpg/04/83/35/33/1000_F_483353394_baks2bsYpfB8muZTnZJpXSUd2OtP2Gdn.jpg'}
+							alt="Sustainable clothing logo"
+							class="max-h-96 w-full object-cover"
+						/>
+					{:else}
+						<img src={data.project?.image} alt="Sustainable clothing logo" class="max-h-96 w-full object-cover" />
+					{/if}
 		</figure>
 		<div class="card-body">
-			<!-- <div class="avatar">
-				<div class="w-16 rounded-full">
-					<img src={data.project.avatar} />
+			<a href={`/@${data.user?.username}`} class="btn">
+				<div class="avatar">
+					<div class="w-8 rounded-full">
+						<img src={data.user?.image} alt="profile" />
+					</div>
 				</div>
-			</div> -->
-			<p>Glen Chaplin, UK, posted on 16/03/2024 at 2:44am</p>
+				<p>{data.user?.name}</p>
+			</a>
 			<h2 class="card-title text-4xl">{data.project?.title}</h2>
 			<p class="text-xl">{data.project?.description}</p>
 		</div>
 	</div>
-	<div class="buttons flex">
-		{#if watching}
-		<button class="btn w-fill" on:click={unwatchProject}>Unwatch</button>
-		{:else}
-		<button class="btn w-fill" on:click={watchProject}>Watch</button>
-		{/if}
-		<a class="btn btn-outline btn-accent" href="#modal">Donate</a>
-	</div>
-	<div id="modal" class="donatePrompt">
-		
-	</div>
-	<p class="text-center text-xl">Supplies needed / already received</p>
-	<div class="card w-3/6 bg-base-100 shadow-xl">
+	{#if watching}
+		<button class="btn max-w-xl w-full" on:click={unwatchProject}>Unwatch</button>
+	{:else}
+		<button class="btn max-w-xl w-full" on:click={watchProject}>Watch</button>
+	{/if}
+	<p class="text-center text-xl">Supplies Needed</p>
+	<div class="card bg-base-100 shadow-xl max-w-xl w-full">
 		<div class="overflow-x-auto">
 			<table class="table">
-			  <!-- head -->
-			  <thead>
-				<tr>
-				  <th>Name</th>
-				  <th>Amount</th>
-				  <th>Needed</th>
-				</tr>
-			  </thead>
-			  <tbody>
-				<!-- row  -->
-				{#each data.project?.items ?? [] as item}
-				<tr>
-				  <td>{item.name}</td>
-				  <td>{item.amount}</td>
-				  <td>
-					{#if item.needed}
-					<input type="checkbox" class="checkbox" disabled checked />
-					{:else}
-					<input type="checkbox" class="checkbox" disabled />
-					{/if}
-				  </td>
-				</tr>
-				{/each}
-			  </tbody>
+				<!-- head -->
+				<thead>
+					<tr>
+						<th>Name</th>
+						<th>Amount</th>
+						<th>Needed</th>
+					</tr>
+				</thead>
+				<tbody>
+					<!-- row  -->
+					{#each data.project?.items ?? [] as item}
+						<tr>
+							<td>{item.name}</td>
+							<td>{item.amount}</td>
+							<td>
+								{#if item.needed}
+									<input type="checkbox" class="checkbox" disabled checked />
+								{:else}
+									<input type="checkbox" class="checkbox" disabled />
+								{/if}
+							</td>
+						</tr>
+					{/each}
+				</tbody>
 			</table>
-		  </div>
+		</div>
 	</div>
 </div>
 
